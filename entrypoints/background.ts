4f413f239ce6166a42a4de3befe733a2b6516564
DraconDev <dracsharp@gmail.com>
2025-02-16 04:03:40 +0000

    feat(background): Update context menu title to  Sam

--- entrypoints/background.ts
+++ entrypoints/background.ts
@@ -1,15 +1,15 @@
 export default defineBackground(() => {
   // Create context menu item
   browser.contextMenus.create({
-    id: 'samai-context-menu',
-    title: 'Samai',
-    contexts: ['all']
+    id: "samai-context-menu",
+    title: "Sam",
+    contexts: ["all"],
   });
 
   // Add click handler for the context menu item
   browser.contextMenus.onClicked.addListener((info, tab) => {
-    if (info.menuItemId === 'samai-context-menu') {
-      console.log('Samai context menu clicked');
+    if (info.menuItemId === "samai-context-menu") {
+      console.log("Samai context menu clicked");
       // Add your desired action here
     }
   });
