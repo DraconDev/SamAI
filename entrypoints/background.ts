cc4538748529612644a39b71f3902b39e3917588
DraconDev <dracsharp@gmail.com>
2025-02-16 04:16:10 +0000

    feat(background): Pass input info to context menu popup

--- entrypoints/background.ts
+++ entrypoints/background.ts
@@ -7,14 +7,32 @@ export default defineBackground(() => {
   });
 
   // Add click handler for the context menu item
-  browser.contextMenus.onClicked.addListener((info, tab) => {
-    if (info.menuItemId === "samai-context-menu") {
-      browser.windows.create({
-        url: browser.runtime.getURL("/context-popup.html"),
-        type: "popup",
-        width: 300,
-        height: 300,
-      });
+  browser.contextMenus.onClicked.addListener(async (info, tab) => {
+    if (info.menuItemId === "samai-context-menu" && tab?.id) {
+      try {
+        // Get input information from the content script
+        const response = await browser.tabs.sendMessage(tab.id, { type: 'getInputInfo' });
+        
+        // Create popup with input data in URL parameters
+        if (response?.messageType === 'inputInfo') {
+          const params = new URLSearchParams({
+            value: response.value || '',
+            placeholder: response.placeholder || '',
+            inputType: response.inputType || '',
+            elementId: response.id || '',
+            elementName: response.name || ''
+          });
+
+          browser.windows.create({
+            url: `${browser.runtime.getURL("/context-popup.html")}?${params.toString()}`,
+            type: "popup",
+            width: 400,
+            height: 300,
+          });
+        }
+      } catch (error) {
+        console.error('Error getting input information:', error);
+      }
     }
   });
 });
