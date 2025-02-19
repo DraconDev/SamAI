2c4d44e1de7effcb48e15d2b4896ea6f6f6d991a
DraconDev <dracsharp@gmail.com>
2025-02-19 19:00:50 +0000

    fix(context-popup): Clear input after generation

--- entrypoints/context-popup/App.tsx
+++ entrypoints/context-popup/App.tsx
@@ -53,7 +53,7 @@ export default function App() {
       console.log("Generated response:", response);
     }
     // TODO: Handle the input
-    setInput("Test");
+    setInput("");
   };
 
   return (
