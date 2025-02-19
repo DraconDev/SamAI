946eae19ef7538df049b8718e2e6dd61e0813b5a
DraconDev <dracsharp@gmail.com>
2025-02-19 19:18:08 +0000

    feat(content): add setInputValue message handler This commit adds a message handler to set the value of the last focused input element and dispatch input/change events.

--- entrypoints/content.ts
+++ entrypoints/content.ts
@@ -37,6 +37,12 @@ export default defineContentScript({
         };
         console.log("Sending input info:", response);
         sendResponse(response);
+      } else if (message.type === "setInputValue" && lastInputElement && message.value) {
+        lastInputElement.value = message.value;
+        lastInputElement.dispatchEvent(new Event('input', { bubbles: true }));
+        lastInputElement.dispatchEvent(new Event('change', { bubbles: true }));
+        console.log("Updated input value:", message.value);
+        sendResponse(true);
       } else {
         console.log("No input element or wrong message type");
         sendResponse(false);
