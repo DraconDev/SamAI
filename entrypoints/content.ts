0114e7772b81b8efd76519f3b92025b1da250958
DraconDev <dracsharp@gmail.com>
2025-02-19 19:32:15 +0000

    feat(content): Track last input element with focus and contextmenu, add tabId

--- entrypoints/content.ts
+++ entrypoints/content.ts
@@ -2,6 +2,7 @@ export default defineContentScript({
   matches: ["<all_urls>"],
   main() {
     let lastInputElement: HTMLInputElement | HTMLTextAreaElement | null = null;
+    const tabId = crypto.randomUUID(); // Generate unique ID for this tab instance
 
     // Listen for right clicks to track the last input element
     document.addEventListener("contextmenu", (event) => {
@@ -11,12 +12,31 @@ export default defineContentScript({
         target instanceof HTMLTextAreaElement
       ) {
         lastInputElement = target;
+        console.log("Input element right-clicked:", lastInputElement);
       } else {
         lastInputElement = null;
       }
     });
 
-    console.log("Content script initialized");
+    // Also track focus events to maintain input reference
+    document.addEventListener("focusin", (event) => {
+      const target = event.target as HTMLElement;
+      if (
+        target instanceof HTMLInputElement ||
+        target instanceof HTMLTextAreaElement
+      ) {
+        lastInputElement = target;
+        console.log("Input element focused:", lastInputElement);
+      }
+    });
+
+    console.log("Content script initialized with tab ID:", tabId);
+
+    // Register with background script
+    browser.runtime.sendMessage({
+      type: "contentScriptReady",
+      tabId: tabId
+    }).catch(console.error);
 
     // Return true from the listener to indicate we want to send a response
     browser.runtime.onMessage.addListener((message, sender, sendResponse) => {
@@ -34,24 +54,5 @@ export default defineContentScript({
             lastInputElement instanceof HTMLInputElement
               ? lastInputElement.type
               : "textarea",
+          tabId: tabId // Include tab ID in response
         };
-        console.log("Sending input info:", response);
-        sendResponse(response);
-      } else if (
-        message.type === "setInputValue" &&
-        lastInputElement &&
-        message.value
-      ) {
-        lastInputElement.value = message.value;
-        lastInputElement.dispatchEvent(new Event("input", { bubbles: true }));
-        lastInputElement.dispatchEvent(new Event("change", { bubbles: true }));
-        console.log("Updated input value:", message.value);
-        sendResponse(true);
-      } else {
-        console.log("No input element or wrong message type");
-        sendResponse(false);
-      }
-      return true; // Will respond asynchronously
-    });
-  },
-});
