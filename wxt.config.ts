3b0af915008efec5dcdbad60b51bf25be5c3afe8
DraconDev <dracsharp@gmail.com>
2025-02-28 07:24:47 +0000

    feat(style): Darken background color

--- wxt.config.ts
+++ wxt.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
     name: "SamAI - Free AI Assistant",
     description: "SamAI - Free AI Assistant",
     manifest_version: 3,
-    version: "1.1.21",
+    version: "1.1.22",
     permissions: ["storage", "contextMenus", "activeTab", "tabs", "scripting"],
     host_permissions: ["<all_urls>"],
     icons: {
