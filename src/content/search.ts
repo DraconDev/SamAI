c86821862f912cf97743a60826a0b6d496690a8b
DraconDev <dracsharp@gmail.com>
2025-11-23 05:52:47 +0000

    fix(search): Remove unused `onSummarize` prop from `SearchPanel`

--- src/content/search.ts
+++ src/content/search.ts
@@ -140,7 +140,6 @@ export async function showSidePanel(
       React.createElement(SearchPanel, {
         response,
         outputFormat,
-        onSummarize: handleSummarize,
         onClose: () => {
           if (samaiRoot) {
             samaiRoot.unmount();
