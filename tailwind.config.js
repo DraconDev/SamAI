a2542778b038a81c533b8f0019dd73164ea7c028
DraconDev <dracsharp@gmail.com>
2025-11-24 10:19:24 +0000

    refactor(css): Remove direct `<link>` to `../style.css` from entrypoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,8 @@
 export default {
   content: [
     "./entrypoints/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
     "./src/**/*.{js,ts,jsx,tsx}",
+    "./utils/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
