64d4d68e3e04650e2b6c1c5cfd9fc636d022e44b
DraconDev <dracsharp@gmail.com>
2025-11-25 20:16:29 +0000

    fix(highlight): Update close icon SVG path

--- utils/searchHighlighter.ts
+++ utils/searchHighlighter.ts
@@ -240,7 +240,7 @@ class SearchHighlighter {
       const hideBtn = document.createElement("button");
       hideBtn.innerHTML = `
         <svg viewBox="0 0 24 24" width="16" height="16" fill="none" stroke="currentColor" stroke-width="2">
-          <path d="M18 6L6 18M6 6l12 12"/>
+          <path d="M21 21L9 9M9 21L21 9"/>
         </svg>
       `;
       hideBtn.title = "Hide this domain";
