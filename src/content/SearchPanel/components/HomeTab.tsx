73a96b2447f416b0ff53e9d2d38300b6ebda3bab
DraconDev <dracsharp@gmail.com>
2025-11-28 04:07:01 +0000

    feat(search): Add functionality to auto-fetch favicon when adding current site

--- src/content/SearchPanel/components/HomeTab.tsx
+++ src/content/SearchPanel/components/HomeTab.tsx
@@ -152,6 +152,7 @@ const HomeTab: React.FC<HomeTabProps> = () => {
     await saveHomeData(newData);
   };
   // Add current site with auto-fetched favicon
+
   const addCurrentSite = async () => {
     const url = window.location.href;
     const name = document.title || "Current Page";
